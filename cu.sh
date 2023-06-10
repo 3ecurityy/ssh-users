8e94bf6983cdac1e4720e7bd4ee1e8bf02eeb83a
MohammadReza <3ecurityy@gmail.com>
2023-06-10 07:46:32 +0330

    Add files via upload

---
+++ cu.sh
@@ -0,0 +1,18 @@
+if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username : " username
+	read -p  "Enter expire date ( y-m-d ) example 2023-06-10 : " date
+	read -s -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p "$pass" "$username"
+		usermod -e  "$date" "$username"
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system."
+	exit 2
+fi
