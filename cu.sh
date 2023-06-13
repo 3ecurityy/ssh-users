2002af321a079c8ee130900b68260c13d5d642e6
MohammadReza <3ecurityy@gmail.com>
2023-06-13 23:55:23 +0330

    Update cu.sh

--- cu.sh
+++ cu.sh
@@ -8,7 +8,7 @@ if [ $(id -u) -eq 0 ]; then
 		exit 1
 	else
 		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		useradd -m -p "$pass" "$username"
+		useradd -m -p "$pass" "$username" --shell=/bin/false
 		usermod -e  "$date" "$username"
 		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
 	fi
