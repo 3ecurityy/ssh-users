2fbc81e2e46ab633e2f1d0b556f0cd178aac1467
MohammadReza <3ecurityy@gmail.com>
2023-06-14 22:34:57 +0330

    add traffic limit

--- cu.sh
+++ cu.sh
@@ -1,17 +1,28 @@
 if [ $(id -u) -eq 0 ]; then
 	read -p "Enter username : " username
-	read -p  "Enter expire date ( y-m-d ) example 2023-06-10 : " date
-	read -s -p "Enter password : " password
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
+	 egrep "^$username" /etc/passwd >/dev/null
+	  if [ $? -eq 0 ]; then
+		echo -e "user $username exists!"
 		exit 1
-	else
+	  fi
+	read -p "Enter expire date ( y-m-d ) example 2023-06-10 : " date
+	read -p "Enter traffic (GB) : " traffic  
+	read -p "Enter password : " password
 		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 		useradd -m -p "$pass" "$username" --shell=/bin/false
 		usermod -e  "$date" "$username"
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-	fi
+	
+		prefix=1000000000
+	        gb=$((traffic * prefix))
+		iptables -A OUTPUT -p tcp -m owner --uid-owner $username -m quota --quota $gb -j ACCEPT
+		[ $? -eq 0 ] &&  echo -e "user added :) "
+    echo -e "----------------------------------------------"
+    echo -e "username : "$username
+    echo -e "expiry date : "$date
+    echo -e "traffic (GB) : "$traffic
+    echo -e "password : "$password
+    echo -e "----------------------------------------------" || echo "Failed to add a user!"
+    
 else
 	echo "Only root may add a user to the system."
 	exit 2
